5abbce4dbb1d5e502391c479a01611aabf2f9a2e
YuvalBubnovsky <yuval.bubnov@gmail.com>
2022-04-03 22:52:28 +0300

    basic header done

--- Matrix.cpp
+++ Matrix.cpp
@@ -1,53 +1,225 @@
 #include "Matrix.hpp"
 #include <cmath>
 
+using std::cout;
+using std::istream;
+using std::ostream;
 
 namespace zich
 {
-    Matrix::Matrix(vector<double> matVector, int rows, int columns)
+
+    int checkPositive(int k)
     {
-        if (rows <= 0 || columns <= 0)
+        if (k <= 0)
         {
-            throw std::invalid_argument("Rows/Columns Must Be A Positive Number!");
+            throw "Invalid Dimensions!";
         }
-        if (matVector.size() != rows * columns)
+        return k;
+    }
+
+    // If we got dimensions in int, cast to size_t
+    Matrix::Matrix(vector<double> vals, int rows, int cols)
+        : Matrix(std::move(vals), static_cast<size_t>(checkPositive(rows)), static_cast<size_t>(checkPositive(cols)))
+    {
+    }
+    Matrix::Matrix(vector<double> mat, size_t rows, size_t columns) : rows{rows}, columns{columns}
+    {
+        if (mat.size() != rows * columns)
         {
-            throw std::invalid_argument("Not Enough Arguments!");
+            throw "Invalid Dimensions!";
         }
-        this->rows = rows;
-        this->columns = columns;
-        this->matrix.resize((unsigned int)rows);
-        for (unsigned int i = 0; i < rows; i++)
+
+        for (int i = 0; i < rows; i++)
         {
-            this->matrix[i].resize((unsigned int)columns);
+            this->matrix.push_back(vector<double>(columns));
         }
-        unsigned int index = 0;
-        for (unsigned int i = 0; i < rows; i++)
+
+        for (size_t i = 0; i < rows * columns; i++)
         {
-            for (unsigned int j = 0; j < columns; j++)
-            {
-                this->matrix[i][j] = matVector[index];
-                index++;
-            }
+            this->matrix.at(i / columns).at(i % columns) = mat.at(i);
         }
     }
 
-    Matrix::Matrix(int rows, int columns){
-        if (rows<=0 || columns<=0){
-            throw std::invalid_argument("Rows/Columns Must Be A Positive Number!");
+    Matrix::Matrix(int rows, int columns)
+    {
+        if ((rows <= 0 || columns <= 0))
+        {
+            throw "Rows/Columns Must Be Positive Number!";
         }
-        this->rows = rows;
-        this->columns = columns;
-        this->matrix.resize((unsigned int)rows);
-        for (unsigned int i=0;i<rows;i++){
-            this->matrix[i].resize((unsigned int)columns);
+        this->rows = static_cast<size_t>(rows);
+        this->columns = static_cast<size_t>(columns);
+        this->matrix.resize(static_cast<size_t>(rows));
+        for (size_t i = 0; i < rows; i++)
+        {
+            this->matrix[i].resize(static_cast<size_t>(columns));
         }
     }
 
-    Matrix::~Matrix(){
-        for(unsigned int i=0;i<this->rows;i++){
+    // Copy Constructor
+    Matrix::Matrix(const Matrix &mat)
+        : rows{mat.rows}, columns{mat.columns}
+    {
+        this->matrix = mat.matrix;
+    }
+
+    Matrix::~Matrix()
+    {
+        for (unsigned int i = 0; i < this->rows; i++)
+        {
             this->matrix[i].clear();
         }
         this->matrix.clear();
+    }
+
+    // Global Operators
+    istream &operator>>(istream &in, const Matrix &mat)
+    {
+        return in;
+    }
+    ostream &operator<<(ostream &out, const Matrix &mat)
+    {
+        return out;
+    }
+
+    // Comparison Operators
+    bool operator>(const Matrix &mat1, const Matrix &mat2)
+    {
+        return false;
+    }
+    bool operator>=(const Matrix &mat1, const Matrix &mat2)
+    {
+        return false;
+    }
+
+    bool operator<(const Matrix &mat1, const Matrix &mat2)
+    {
+        return false;
+    }
+    bool operator<=(const Matrix &mat1, const Matrix &mat2)
+    {
+        return false;
+    }
+
+    bool operator==(const Matrix &mat1, const Matrix &mat2)
+    {
+        return false;
+    }
+    bool operator!=(const Matrix &mat1, const Matrix &mat2)
+    {
+        return false;
+    }
+
+    // Single Matrix Operators
+    Matrix operator+(const Matrix &mat, double number)
+    {
+        return Matrix(2, 2);
+    }
+    Matrix operator+(double number, const Matrix &mat)
+    {
+        return Matrix(2, 2);
+    }
+    Matrix operator+(const Matrix &mat)
+    {
+        return Matrix(2, 2);
+    }
+    void operator+=(Matrix &mat, double number)
+    {
+    }
+
+    Matrix operator-(const Matrix &mat, double number)
+    {
+        return Matrix(2, 2);
+    }
+    Matrix operator-(double number, const Matrix &mat)
+    {
+        return Matrix(2, 2);
+    }
+    Matrix operator-(const Matrix &mat)
+    {
+        return Matrix(2, 2);
+    }
+    void operator-=(Matrix &mat, double number)
+    {
+    }
+
+    Matrix operator*(double number, const Matrix &mat)
+    {
+        return Matrix(2, 2);
+    }
+    Matrix operator*(const Matrix &mat, double number)
+    {
+        return Matrix(2, 2);
+    }
+    void operator*=(Matrix &mat, double number)
+    {
+    }
+
+    void operator++(Matrix &mat)
+    {
+    }
+    void operator--(Matrix &mat)
+    {
+    }
+
+    // Double Matrix Operators
+    Matrix operator+(const Matrix &mat1, const Matrix &mat2)
+    {
+        return Matrix(2, 2);
+    }
+    void operator+=(Matrix &mat1, const Matrix &mat2)
+    {
+    }
+
+    Matrix operator-(const Matrix &mat1, const Matrix &mat2)
+    {
+        return Matrix(2, 2);
+    }
+    void operator-=(Matrix &mat1, const Matrix &mat2)
+    {
+    }
+
+    Matrix operator*(const Matrix &mat1, const Matrix &mat2)
+    {
+        return Matrix(2, 2);
+    }
+    void operator*=(Matrix &mat1, const Matrix &mat2)
+    {
+    }
+
+    // Integer Operators
+    Matrix operator+(const Matrix &mat, int number)
+    {
+        return Matrix(2, 2);
+    }
+    Matrix operator+(int number, const Matrix &mat)
+    {
+        return Matrix(2, 2);
+    }
+    void operator+=(Matrix &mat, int number)
+    {
+    }
+
+    Matrix operator-(const Matrix &mat, int number)
+    {
+        return Matrix(2, 2);
+    }
+    Matrix operator-(int number, const Matrix &mat)
+    {
+        return Matrix(2, 2);
+    }
+    void operator-=(Matrix &mat, int number)
+    {
+    }
+
+    Matrix operator*(int number, const Matrix &mat)
+    {
+        return Matrix(2, 2);
+    }
+    Matrix operator*(const Matrix &mat, int number)
+    {
+        return Matrix(2, 2);
+    }
+    void operator*=(Matrix &mat, int number)
+    {
     }
 }
--- Matrix.hpp
+++ Matrix.hpp
@@ -1,5 +1,7 @@
+#pragma once
 #include <iostream>
 #include <vector>
+#include <string>
 
 using std::istream;
 using std::ostream;
@@ -10,17 +12,61 @@ namespace zich
     class Matrix
     {
     private:
-        int rows;
-        int columns;
+        size_t rows;
+        size_t columns;
         vector<vector<double>> matrix;
 
     public:
-
         // Constructors
-        Matrix(vector<double> matVector, int rows, int columns);
+        Matrix(vector<double> mat, size_t rows, size_t columns);
+        Matrix(vector<double> mat, int rows, int columns);
+        Matrix(const Matrix &mat);
         Matrix(int rows, int columns);
 
         // Destructor
         ~Matrix();
+
+        // Global Operators
+        friend istream &operator>>(istream &in, const Matrix &mat);
+        friend ostream &operator<<(ostream &out, const Matrix &mat);
+
+        // Comparison Operators
+        friend bool operator>(const Matrix &mat1, const Matrix &mat2);
+        friend bool operator>=(const Matrix &mat1, const Matrix &mat2);
+
+        friend bool operator<(const Matrix &mat1, const Matrix &mat2);
+        friend bool operator<=(const Matrix &mat1, const Matrix &mat2);
+
+        friend bool operator==(const Matrix &mat1, const Matrix &mat2);
+        friend bool operator!=(const Matrix &mat1, const Matrix &mat2);
+
+        // Single Matrix Operators
+        friend Matrix operator+(const Matrix &mat, double number);
+        friend Matrix operator+(double number, const Matrix &mat);
+        friend Matrix operator+(const Matrix &mat);
+        friend void operator+=(Matrix &mat, double number);
+
+        friend Matrix operator-(const Matrix &mat, double number);
+        friend Matrix operator-(double number, const Matrix &mat);
+        friend Matrix operator-(const Matrix &mat);
+        friend void operator-=(Matrix &mat, double number);
+
+        friend Matrix operator*(double number, const Matrix &mat);
+        friend Matrix operator*(const Matrix &mat, double number);
+        friend void operator*=(Matrix &mat, double number);
+
+        friend void operator++(Matrix &mat);
+        friend void operator--(Matrix &mat);
+
+        // Double Matrix Operators
+        friend Matrix operator+(const Matrix &mat1, const Matrix &mat2);
+        friend void operator+=(Matrix &mat1, const Matrix &mat2);
+
+        friend Matrix operator-(const Matrix &mat1, const Matrix &mat2);
+        friend void operator-=(Matrix &mat1, const Matrix &mat2);
+
+        friend Matrix operator*(const Matrix &mat1, const Matrix &mat2);
+        friend void operator*=(Matrix &mat1, const Matrix &mat2);
+
     };
 };
